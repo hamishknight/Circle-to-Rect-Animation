0a4933b244d050a8273a755ff7c023e7416d6499
Hamish Knight <hk@southerncare.co.uk>
2016-01-27 11:48:41 +0000

    initial commit

--- Circle to Rect Animation/ViewController.swift
+++ Circle to Rect Animation/ViewController.swift
@@ -10,16 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let animLayer = CALayer() // the layer that is going to be animated
+    let cornerRadiusAnim = CABasicAnimation(keyPath: "cornerRadius") // the corner radius reducing animation
+    let cornerRadiusUndoAnim = CABasicAnimation(keyPath: "cornerRadius") // the corner radius increasing animation
+    let widthAnim = CABasicAnimation(keyPath: "bounds.size.width") // the width animation
+    let animDuration = NSTimeInterval(1.0) // the duration of one 'segment' of the animation
+    let layerSize = CGFloat(100) // the width & height of the layer
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let rect = view.frame
+        
+        animLayer.backgroundColor = UIColor.blueColor().CGColor // color of the layer, feel free to change
+        animLayer.frame = CGRect(x: rect.width-layerSize*0.5, y: rect.height-layerSize*0.5, width: layerSize, height: layerSize)
+        animLayer.cornerRadius = layerSize*0.5;
+        animLayer.anchorPoint = CGPoint(x: 1, y: 1) // sets so that when the width is changed, it goes to the left
+        view.layer.addSublayer(animLayer)
+        
+        // decreases the corner radius
+        cornerRadiusAnim.duration = animDuration
+        cornerRadiusAnim.fromValue = animLayer.cornerRadius
+        cornerRadiusAnim.toValue = 0;
+        
+        // inverse of the cornerRadiusAnim
+        cornerRadiusUndoAnim.duration = animDuration
+        cornerRadiusUndoAnim.fromValue = 0;
+        cornerRadiusUndoAnim.toValue = animLayer.cornerRadius
+        
+        // increases the width, and autoreverses on completion
+        widthAnim.duration = animDuration
+        widthAnim.fromValue = animLayer.frame.size.width
+        widthAnim.toValue = rect.size.width
+        widthAnim.autoreverses = true
+        widthAnim.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+        
+        animLayer.addAnimation(cornerRadiusUndoAnim, forKey: "cornerRadiusUndo")
+        
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        animLayer.cornerRadius = layerSize*0.5
+        CATransaction.commit()
     }
-
-
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        widthAnim.beginTime = CACurrentMediaTime()+animDuration // starts after the corner radius anim has finished
+        
+        animLayer.addAnimation(widthAnim, forKey: "widthAnim")
+        animLayer.addAnimation(cornerRadiusAnim, forKey: "cornerRadius")
+        
+        CATransaction.begin()
+        CATransaction.setDisableActions(true) // disables implicit animations
+        animLayer.cornerRadius = 0
+        CATransaction.commit()
+    }
 }
 
